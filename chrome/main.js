5b31e222bd9158f6e729d0eee4f6898b3ff0788f
SpaceshipCaptain <64009670+SpaceshipCaptain@users.noreply.github.com>
2022-03-03 00:39:53 -0600

    general cleanup and fixed some bugs

--- chrome/main.js
+++ chrome/main.js
@@ -1,16 +1,15 @@
 console.log('TTVLookingGlass Extension Initiated. Created by @SpaceshipCapt')
 //chrome 
 
-let gob = {}; //global object because I'm lazy
-let inputed = []; //array of successful inputs
-let lasturl;
+var gob = {}; //global object because I'm lazy
+var inputed = []; //array of successful inputs
 
 //target query divs for extention embed
-let clipsdot = "div.Layout-sc-nxg1ff-0.eYzYtX"; //clips.twitch.tv selects div below and inserts above
-let slashclips = "div.Layout-sc-nxg1ff-0.hhrWzV";  //twitch.tv/moonmoon/clips selects div below and inserts above
-let slashvideos = "div.Layout-sc-nxg1ff-0.hhrWzV"; //twitch.tv/video/numbers selects div below and inserts above
-let playertime = '.CoreText-sc-cpl358-0.cKlVwN[data-a-target="player-seekbar-current-time"]'; //.innertext of this selects the current vod time
-let expresstarget = 'div.Layout-sc-nxg1ff-0.cpmuUc'; //express vod target left of prime loot crown
+const clipsdot = "div.Layout-sc-nxg1ff-0.eYzYtX"; //clips.twitch.tv selects div below and inserts above
+const slashclips = "div.Layout-sc-nxg1ff-0.hhrWzV";  //twitch.tv/moonmoon/clips selects div below and inserts above
+const slashvideos = "div.Layout-sc-nxg1ff-0.hhrWzV"; //twitch.tv/video/numbers selects div below and inserts above
+const playertime = '.CoreText-sc-cpl358-0.cKlVwN[data-a-target="player-seekbar-current-time"]'; //.innertext of this selects the current vod time
+const expresstarget = 'div.Layout-sc-nxg1ff-0.cpmuUc'; //express vod target left of prime loot crown
 
 //on load start looping
 window.onload=() =>{ 
@@ -19,31 +18,28 @@ window.onload=() =>{
 function looper() {
 //console.log("looper")
     setTimeout(() => {
-        if(document.querySelector(expresstarget) != null && (document.getElementById('expressvod'))== null){ //checks to see if element is there and then generates if it isn't
-                expressvod();
+        if(document.querySelector(expresstarget) != null && (document.getElementById('expressvod'))=== null){ //checks to see if element is there and then generates if it isn't
+                expressvod();}
+        if((document.querySelector(clipsdot) || document.querySelector(slashclips) || document.querySelector(slashvideos) != null)){ //checks if any of the selectors are on page
+            if(document.getElementById('finderwrap') != null && (document.querySelector("#finderwrap").classList[0] != window.location.pathname.substring(window.location.pathname.lastIndexOf("/") + 1))){
+                document.getElementById('finderwrap').remove()} //checks if box exists and if the box class does not match url it removes the box
+            if(document.getElementById('finderwrap') === null){
+                urlchecker();} //if the box does not exist check url and probably create one
+            else{looper();}
         }
-        if (lasturl === window.location.href){ //if the url has not changed don't do anything
-            if(window.location.pathname.includes("/clip/","/videos/") || window.location.hostname === "clips.twitch.tv"){
-                if(document.getElementById('finderwrap') === null){urlchecker();}
-            }
-        looper();
-        }
-        if(lasturl != window.location.href){ //if the url has changed check it and maybe do stuff
-            urlchecker();
-        }
+        else{looper();}
     }, 3000);
 }
 
 function urlchecker(){
 //console.log("checker");
-    lasturl = window.location.href;
     if(window.location.pathname.includes("/clip/") || window.location.hostname === "clips.twitch.tv"){ //if it's a clip use clipsetup
         gob.type = "clip";
-        setTimeout(() => {clipsetup()},500);
+        clipsetup()
         looper();
     } else if(window.location.pathname.includes("/videos/") || window.location.pathname.includes("/video/")){ //if vod use vodsetup
         gob.type = "vod";
-        setTimeout(() => {vodsetup()},500);
+        vodsetup()
         looper();
     } else{
         if(document.getElementById('finderwrap') != null){(document.getElementById('finderwrap')).remove()}; //if navigating somewhere else delete finder because edgecases
@@ -74,7 +70,6 @@ const apireturnexpress = async (input) => { //user input to get single video lin
     if(a.user.videos.edges.length === 0){return}; //if user exists but has no videos just exit
     var link =("https://twitch.tv/videos/").concat(a.user.videos.edges[0].node.id);
     window.open(link, "_blank");
-    window.focus();
 };
 
 //gql queries //moreinformation here https://supersonichub1.github.io/twitch-graphql-api/
@@ -85,9 +80,7 @@ function videoquery(input){ //video id input gets video info back
               createdAt
               lengthSeconds
               id
-              title
-              createdAt
-              creator {displayName}
+              broadcastType
         }
       }`);
       return videoq
@@ -121,10 +114,6 @@ function clipquery(input){ //clip slug input and gets clip info back
             video{
               id
               createdAt
-              duration
-            }
-            broadcaster {
-              login 
             }
         }
         }`)
@@ -138,21 +127,26 @@ const apifetch = async (input) => {
       body: JSON.stringify({query: input}),
       headers: {"Client-Id": "kimne78kx3ncx6brgo4mv6wki5h1ko",}
     })
+    return gqlfetch
       .then(response => response.json())
       .then((data) => {
         return data.data;
       });
-    return gqlfetch
   };
 
 const apireturnv = async (input) => { //video information api call
     const a = await apifetch(input);
-    if(a.video != null){
-        gob.vstime = Date.parse(a.video.createdAt)/1000 //start time of vod
-    }
-    else{
+    if(a.video === null){
         document.getElementById('infodiv').innerText = "API didn't return vod info. If this problem persists contact dev->@SpaceshipCapt"
         document.getElementById('infodiv').style.color = "#D68029";
+    }
+    if(a.video.broadcastType != "ARCHIVE" && a.video.broadcastType != null){
+        document.getElementById('infodiv').innerText = "This is not a vod. Looking Glass will not function properly."
+        document.getElementById('infodiv').style.color = "#D68029";
+        gob.vstime = 804643200 //this is just to have an old starttime to not confuse user
+    }
+    else{
+        gob.vstime = Date.parse(a.video.createdAt)/1000 //start time of vod
     }
 };
 
@@ -171,16 +165,24 @@ const apireturnc = async (input) => { //clip information api call
 const apireturnu = async (input) => { //user input to get list of videos api call
     const a = await apifetch(input);
     if(a.user == null){document.getElementById('infodiv').innerText = "Invalid Twitch Name. Try Again."; return;}
+    if(a.user.videos.edges.length === 0){
+        document.getElementById('infodiv').innerText = `User has no vods.`;
+        document.getElementById('infodiv').style.color = "gray";
+        return
+    }
+    document.getElementById('infodiv').style.color = "white";
     gob.varray = a.user.videos.edges;
     arrayvods()
 };
 
 function boxcreate(qselector){
 //console.log('boxcreate')
-    let target = document.querySelector(qselector)
-    let cdiv = document.createElement('div') 
+    var target = document.querySelector(qselector)
+    var cdiv = document.createElement('div') 
     var created = target.parentNode.insertBefore(cdiv, target)
     created.setAttribute("id", "finderwrap");
+    created.setAttribute("class",  window.location.pathname.substring(window.location.pathname.lastIndexOf("/") + 1));
+//above creates a class with url pathname select this with document.querySelector("#finderwrap").classList[0]
     var top = document.getElementById('finderwrap').appendChild(document.createElement('div'))
     top.setAttribute("id", "topd")
     var bot = document.getElementById('finderwrap').appendChild(document.createElement('div'))
@@ -202,8 +204,8 @@ function boxcreate(qselector){
     var createinfod = document.getElementById('topd').appendChild(document.createElement('div'))
     createinfod.setAttribute("id", "infodiv")
     createinfod.innerText = "Submit a name to get their perspective.";
-
-    let evel = document.getElementById('targetid')
+//event listeners for submitting names
+    var evel = document.getElementById('targetid')
     evel.addEventListener("keydown", (event) =>{
         if(event.defaultPrevented){return;}
 
@@ -219,7 +221,6 @@ function boxcreate(qselector){
 
 function clipsetup(){
 //console.log('clipsetup');
-    if(document.getElementById('finderwrap') != null){(document.getElementById('finderwrap')).remove()}; //if the input div already exists delete it
     inputed = []; //resets the inputs for rare cases
     if(window.location.hostname == "clips.twitch.tv"){//clips.twitch.tv/blahdbladhbladh view
         boxcreate(clipsdot);
@@ -227,16 +228,14 @@ function clipsetup(){
     if(window.location.hostname == "www.twitch.tv"){ //channel clips view eg twitch.tv/moonmoon/clips/blahblahblah
         boxcreate(slashclips);
     };
-    let cslug = window.location.pathname.substring(window.location.pathname.lastIndexOf("/") + 1);
-    apireturnc(clipquery(cslug)); 
+    apireturnc(clipquery(window.location.pathname.substring(window.location.pathname.lastIndexOf("/") + 1))); 
 } 
 
 function vodsetup(){
-    if(document.getElementById('finderwrap') != null){(document.getElementById('finderwrap')).remove()}; //if the input div already exists delete it
-    boxcreate(slashvideos);
+//console.log('vodsetup');
     inputed = []; //resets the inputs for rare cases
-    let videoid = window.location.pathname.substring(window.location.pathname.lastIndexOf("/") + 1)
-    apireturnv(videoquery(videoid))
+    boxcreate(slashvideos);
+    apireturnv(videoquery(window.location.pathname.substring(window.location.pathname.lastIndexOf("/") + 1)))
 }
 function start(){
 //console.log('start')
@@ -267,6 +266,9 @@ function getinput(){
     var iname =  (document.getElementById('targetid').value).replace(/\W/g, '');//clears the input of non alphanumeric characters
     if(iname === ""){document.getElementById('infodiv').innerText = "Empty input. Enter name.";return};
     if(inputedtest(iname) === true){document.getElementById('infodiv').innerText = "Repeat entry.";return};
+    if(gob.type === "vod"){
+        if(inputedtest(iname.concat(" "+document.body.querySelector(playertime).innerText)) === true){document.getElementById('infodiv').innerText = "Repeat entry.";return}
+    }
     document.getElementById('targetid').value = ""; //clears input box
     return iname;
 }
@@ -282,7 +284,7 @@ function arrayvods(){
     }
     if(gob.stime < vodstart[vodstart.length-1]){
         gob.color = "gray";
-        document.getElementById('infodiv').innerText = `Searched ${gob.varray.length} vods and this is older than all of them`;
+        document.getElementById('infodiv').innerText = `Searched ${gob.varray.length} vod(s) and this is older than all of them`;
         cl();
         return 
     }
@@ -293,14 +295,14 @@ function arrayvods(){
                 gob.link = ("https://twitch.tv/videos/").concat((gob.varray[i].node.id).concat("?t=")).concat(ts)
                 gob.color="green";
                 cl();
-                document.getElementById('infodiv').innerText = `${gob.varray.length} vods and this was found ${i+1} vods ago!`
+                document.getElementById('infodiv').innerText = `${gob.varray.length} vod(s) and this was found ${i+1} vod(s) ago!`
                 return 
             }
         }
         gob.color="red";
         gob.link = "novod";
         cl();
-        document.getElementById('infodiv').innerText = `${gob.varray.length} vods and this timestamp wasn't found in any of them.`;
+        document.getElementById('infodiv').innerText = `${gob.varray.length} vod(s) and this timestamp wasn't found in any of them.`;
     }
 }
 
